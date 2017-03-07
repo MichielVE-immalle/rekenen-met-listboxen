1c6a5beb777c37a020d4a3df62111f8d9ac051e0
MichielVE-immalle <michiel.vaneester@immalle.be>
2017-03-07 18:25:23 +0100

    uploaded files

---
+++ rekenen met listbox/rekenen met listbox/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rekenen_met_listbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            addTextBox.Focus();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem newitem = new ListBoxItem();
+            newitem.Content = addTextBox.Text;
+            numberListBox.Items.Add(newitem);
+            addTextBox.Clear();
+            addTextBox.Focus();
+        }
+
+        private void sumButton_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = 0;
+            int number;
+            //ListBoxItem item;
+
+            foreach (ListBoxItem item in numberListBox.Items)
+            {
+                sum += Convert.ToInt32(item.Content);
+            }
+            sumTextBox.Text = Convert.ToString(sum);
+
+            //for (int index = 0; index < numberListBox.Items.Count; index++)
+            //{
+            //    item = (ListBoxItem)numberListBox.Items[index];
+            //    number = Convert.ToInt32(item.Content);
+            //    sum += number;
+            //}
+            //sumTextBox.Text = Convert.ToString(sum);
+        }
+
+        private void largestButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = (ListBoxItem)numberListBox.Items[0];
+            int largest = Convert.ToInt32(item.Content);
+            int number;
+
+            for (int index = 1; index < numberListBox.Items.Count; index++)
+            {
+                item = (ListBoxItem)numberListBox.Items[index];
+                number = Convert.ToInt32(item.Content);
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+            largestTextBox.Text = Convert.ToString(largest);
+        }
+    }
+}
